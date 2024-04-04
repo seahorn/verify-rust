37604b0c9f5f4398319b9bac9aa7f8cf8ebac062
Adrian Jendo <ajendo@uwaterloo.ca>
2024-04-04 11:20:45 -0400

    Add seamock implementation to verify-rust

---
+++ src/seamock-lib/src/lib.rs
@@ -0,0 +1,249 @@
+extern crate proc_macro;
+
+use proc_macro::TokenStream;
+use quote::__private::Span;
+use quote::quote;
+use syn::{parse_macro_input, ItemTrait, TraitItem, Ident, parse_quote, TraitItemMethod};
+
+fn generate_attr_names(method: &TraitItemMethod, prefixes: &[&str]) -> Vec<Ident> {
+    prefixes
+        .iter()
+        .map(|prefix| Ident::new(&format!("{}_{}", prefix, &method.sig.ident), method.sig.ident.span()))
+        .collect()
+}
+
+#[proc_macro_attribute]
+pub fn seamock(_args: TokenStream, input: TokenStream) -> TokenStream {
+    // Parse the input trait
+    let input = parse_macro_input!(input as ItemTrait);
+
+    let mock_struct_name = Ident::new(&format!("Mock{}", &input.ident), Span::call_site());
+
+    let trait_methods = input.items.iter().filter_map(|item| {
+        if let TraitItem::Method(method) = item {
+            Some(method)
+        } else {
+            None
+        }
+    });
+
+    let max_times = trait_methods.clone().flat_map(|method| {
+        generate_attr_names(method, &["max_times"])
+    });
+
+    let times = trait_methods.clone().flat_map(|method| {
+        generate_attr_names(method, &["times"])
+    });
+
+    let mut returning_attrs = vec!{};
+    let mut with_attrs = vec!{};
+    let mut with_methods = vec!{};
+
+    let ret = trait_methods.clone().flat_map(|method| {
+        let method_output = &method.sig.output;
+        let mut method_inputs = method.sig.inputs.clone();
+        let mut val_with_params = vec![];
+        // Check if the first argument is `&self` and remove it
+        if let Some(first_arg) = method_inputs.first() {
+            if let syn::FnArg::Receiver(_) = first_arg {
+                method_inputs = method_inputs.iter().skip(1).cloned().collect();
+            }
+        }
+        // For each argument, create WithVal<T> where T is the argument type
+        for arg in method_inputs.iter() {
+            if let syn::FnArg::Typed(pat_type) = arg {
+                let arg_type = &pat_type.ty;
+                let with_val_type: syn::Type = parse_quote! { WithVal<#arg_type> };
+                val_with_params.push(with_val_type);
+            }
+        }
+        // Create a tuple of WithVal<T> for each argument
+        let val_with_tuple = if val_with_params.is_empty() {
+            None
+        } else {
+            Some(quote! { ( #(#val_with_params),* ) })
+        };
+        let returning_attr = Ident::new(&format!("val_returning_{}", &method.sig.ident), method.sig.ident.span());
+        let with_attr = Ident::new(&format!("val_with_{}", &method.sig.ident), method.sig.ident.span());
+        let with_method = Ident::new(&format!("with_{}", &method.sig.ident), method.sig.ident.span());
+        returning_attrs.push(quote! {
+            #returning_attr: |#method_inputs| Default::default(),
+        });
+        if val_with_tuple.is_some() {
+            with_attrs.push(quote! {
+                #with_attr: None,
+            });
+            with_methods.push(quote! {
+                fn #with_method(&mut self, with: #val_with_tuple) -> &mut Self {
+                    self.#with_attr = Some(with);
+                    self
+                }
+            });
+            quote! {
+                #returning_attr: fn(#method_inputs) #method_output,
+                #with_attr: Option<#val_with_tuple>,
+            }
+        } else {
+            quote! {
+                #returning_attr: fn(#method_inputs) #method_output,
+            }
+        }
+    });
+
+    let ret_methods = trait_methods.clone().flat_map(|method| {
+        let method_output = &method.sig.output;
+        let mut method_inputs = method.sig.inputs.clone();
+        // Remove `&self`
+        if let Some(first_arg) = method_inputs.first() {
+            if let syn::FnArg::Receiver(_) = first_arg {
+                method_inputs = method_inputs.iter().skip(1).cloned().collect();
+            }
+        }
+        let input_method = Ident::new(&format!("returning_{}", &method.sig.ident), method.sig.ident.span());
+        let update_attr = Ident::new(&format!("val_returning_{}", &method.sig.ident), method.sig.ident.span());
+        Some (quote! {
+            fn #input_method(&mut self, f: fn(#method_inputs) #method_output) -> &mut Self {
+                self.#update_attr = f;
+                self
+            }
+        })
+    });
+
+    let times_methods = trait_methods.clone().flat_map(|method| {
+        let times_method = Ident::new(&format!("times_{}", &method.sig.ident), method.sig.ident.span());
+        let update_attr = Ident::new(&format!("max_times_{}", &method.sig.ident), method.sig.ident.span());
+        Some (quote! {
+            fn #times_method(&mut self, val: u64) -> &mut Self {
+                self.#update_attr = val;
+                self
+            }
+        })
+    });
+
+    let expect_times_methods = trait_methods.clone().flat_map(|method| {
+        let times_method = Ident::new(&format!("expect_times_{}", &method.sig.ident), method.sig.ident.span());
+        let times_attr = Ident::new(&format!("times_{}", &method.sig.ident), method.sig.ident.span());
+        Some (quote! {
+            fn #times_method(&mut self, val: u64) -> bool {
+                *self.#times_attr.borrow() == val
+            }
+        })
+    });
+
+    let methods_impl = trait_methods.clone().flat_map(|method| {
+        let method_name =  &method.sig.ident;
+        let method_output = &method.sig.output;
+        let method_inputs = &method.sig.inputs;
+        let mut params = vec!{};
+        // For each argument, create WithVal<T> where T is the argument type
+        for arg in method_inputs.iter() {
+            if let syn::FnArg::Typed(pat_type) = arg {
+                let arg_name = match &*pat_type.pat {
+                    syn::Pat::Ident(ident) => ident.ident.clone(),
+                    _ => unimplemented!(), // Handle other patterns as needed
+                };
+                params.push(quote! { #arg_name, });
+            }
+        }
+
+        let times_attr = Ident::new(&format!("times_{}", &method.sig.ident), method.sig.ident.span());
+        let max_times_attr = Ident::new(&format!("max_times_{}", &method.sig.ident), method.sig.ident.span());
+        let val_attr = Ident::new(&format!("val_with_{}", &method.sig.ident), method.sig.ident.span());
+        let ret_func = Ident::new(&format!("val_returning_{}", &method.sig.ident), method.sig.ident.span());
+        let method_name_string =  &method.sig.ident.to_string();
+
+        // TODO: generate code to check parameter values are correct
+        // let x = if !method_inputs.is_empty() {
+        //     Some(quote! {
+        //         match self.#val_attr {
+        //             Some(x) => {
+        //
+        //                 assert!(val > z);
+        //             }
+        //             Some(WithVal::Gte(val)) {
+        //                 assert!(val >= z);
+        //             }
+        //             Some(WithVal::Eq(val)) => {
+        //                 assert_eq!(val, z);
+        //             }
+        //             None => {}
+        //             _ => {}
+        //         }
+        //     })
+        // } else {
+        //     None
+        // };
+
+        Some (quote! {
+            fn #method_name(#method_inputs) #method_output {
+                self.#times_attr.replace_with(|&mut old| old + 1);
+                // sassert
+                // verifier error
+                if *self.#times_attr.borrow() > self.#max_times_attr {
+                    // panic!("{} called more than {} times", #method_name_string, self.#max_times_attr);
+                }
+                // #x
+                (self.#ret_func)(#(#params)*)
+            }
+        })
+    });
+
+    let times_clone = times.clone();
+    let max_times_clone = max_times.clone();
+
+    // Generate the Mock struct
+    let mock_struct = quote! {
+        struct #mock_struct_name {
+            #(
+                #max_times_clone: u64,
+            )*
+            #(
+                #times_clone: RefCell<u64>,
+            )*
+            #(
+                #ret
+            )*
+        }
+    };
+
+    let times_clone = times.clone();
+    let max_times_clone = max_times.clone();
+
+    // Implement the trait for MockContext
+    let mock_impl = quote! {
+        impl #mock_struct_name {
+            pub fn new() -> Self {
+                Self {
+                    #(
+                        #max_times_clone: u64::MAX,
+                    )*
+                    #(
+                        #times_clone: RefCell::new(0),
+                    )*
+                    #(
+                        #returning_attrs
+                    )*
+                    #(
+                        #with_attrs
+                    )*
+                }
+            }
+            #(#ret_methods)*
+            #(#with_methods)*
+            #(#times_methods)*
+            #(#expect_times_methods)*
+            #(#methods_impl)*
+        }
+    };
+
+    // Combine the generated tokens
+    let expanded = quote! {
+        use core::cell::RefCell;
+        #input
+        #mock_struct
+        #mock_impl
+    };
+
+
+    TokenStream::from(expanded)
+}
