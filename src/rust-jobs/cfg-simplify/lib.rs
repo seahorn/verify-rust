831299dc244ea47bc987d8aa7d6b962d0ec377aa
Siddharth Priya <s2priya@uwaterloo.ca>
2025-04-26 17:57:18 -0400

    fix: enable panic_error for cfg-simplify

--- src/rust-jobs/cfg-simplify/lib.rs
+++ src/rust-jobs/cfg-simplify/lib.rs
@@ -1,28 +1,23 @@
-// Seahorn: This test will fail because all assertions will be discharged by frontend before
-// hitting BMC.
 #![cfg_attr(not(kani), no_std)]
-pub use verifier;
 
-// sea::define_sea_nd!(sea_nd_u8, u8, 42);
+pub use verifier;
 
 #[no_mangle]
 pub extern "C" fn entrypt() {
-    test_simplify_cfg();
+  test_simplify_cfg();
 }
 
 #[cfg_attr(kani, kani::proof)]
 #[cfg_attr(kani, kani::unwind(3))]
-#[cfg_attr(kani, kani::should_panic)]
 fn test_simplify_cfg() {
-    let v: u8 = verifier::any!();
-    verifier::assume!(v < 3);   
+  let v: u8 = verifier::any!();
+  verifier::assume!(v < 3);   
 
-    for i in 0..v as usize {
-      panic!();
-      verifier::error!();
-    }
+  for i in 0..v as usize {
+    return;
+  }
 
-    let mut sentinel: u32 = verifier::any!();
-    // INV: v == 0 => sentinel >= 0 
-    verifier::vassert!(sentinel >= (v as u32)*(v as u32));
+  let mut sentinel: u32 = verifier::any!();
+  // INV: v == 0 => sentinel >= 0 
+  verifier::vassert!(sentinel >= (v as u32)*(v as u32));
 }
