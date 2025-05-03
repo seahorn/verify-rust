18cee574439d7ea075f4b5937508e7c948b19273
Siddharth Priya <s2priya@uwaterloo.ca>
2025-05-03 17:06:29 -0400

    feat: option-vec-map
    simplified by seahorn before hitting bmc. May have type error in vac mode

--- src/rust-jobs/option-vec-map/lib.rs
+++ src/rust-jobs/option-vec-map/lib.rs
@@ -3,21 +3,22 @@ pub use verifier;
 extern crate alloc;
 use alloc::vec::Vec;
 
-// sea::define_sea_nd!(sea_nd_u8, u8, 42);
-
 #[no_mangle]
-#[cfg_attr(kani, kani::proof)]
-#[cfg_attr(kani, kani::unwind(10))]
 pub extern "C" fn entrypt() {
-    // let v: u8 = sea_nd_u8();
+    test();
+}
+
+#[cfg_attr(kani, kani::proof)]
+#[cfg_attr(kani, kani::unwind(3))]
+#[cfg_attr(kani, kani::should_panic)]
+fn test() {
     let v: u8 = verifier::any!();
+    verifier::assume!(v < 3);   
+    let mut nums: Vec<Option<u32>> = Vec::with_capacity(v as usize);
 
-    let capacity: usize = v as usize;
-    let mut nums: Vec<Option<u32>> = Vec::with_capacity(capacity);
-
-    for i in 1..=capacity {
-        let value: u32 = i as u32 - 1;
-        nums[i - 1] = Some(value);
+    for i in 0..v as usize {
+        let value: u32 = 1 + i as u32;
+        nums[i] = Some(value);
     }
 
     let result: Vec<Option<u32>> = nums.into_iter().map(square).collect();
@@ -28,8 +29,9 @@ pub extern "C" fn entrypt() {
             sum += x;
         }
     }
-
+ 
     verifier::vassert!(sum >= (v as u32)*(v as u32));
+
 }
 
 fn square(val: Option<u32>) -> Option<u32> {
